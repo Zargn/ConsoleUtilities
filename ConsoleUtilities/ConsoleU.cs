2d6f1b0f80fb4a5407b695bde1cfb8a55922f59a
Oskar Anderson <oskaranderson99@gmail.com>
2022-09-25 17:10:58 +0200

    WriteWithColor method implemented.
    Again just a Console.Write but with color options in the method call.

--- ConsoleUtilities/ConsoleU.cs
+++ ConsoleUtilities/ConsoleU.cs
@@ -5,13 +5,26 @@ public static class ConsoleU
     /// <summary>
     /// Writes the specified string value, followed by the current line terminator, to the standard output stream in the color specified.
     /// </summary>
-    /// <param name="str">value – The value to write.</param>
+    /// <param name="value">value – The value to write.</param>
     /// <param name="color">color - The color to write in.</param>
     public static void WriteLineWithColor(string? value, ConsoleColor color)
     {
         var oldColor = Console.ForegroundColor;
         Console.ForegroundColor = color;
         Console.WriteLine(value);
+        Console.ForegroundColor = oldColor;
+    }
+    
+    /// <summary>
+    /// Writes the specified string value to the standard output stream in the color specified.
+    /// </summary>
+    /// <param name="value">value – The value to write.</param>
+    /// <param name="color">color - The color to write in.</param>
+    public static void WriteWithColor(string? value, ConsoleColor color)
+    {
+        var oldColor = Console.ForegroundColor;
+        Console.ForegroundColor = color;
+        Console.Write(value);
         Console.ForegroundColor = oldColor;
     }
 }
--- Tester/Program.cs
+++ Tester/Program.cs
@@ -5,5 +5,7 @@ public class Program
     public static void Main()
     {
         ConsoleU.WriteLineWithColor("Hello world!", ConsoleColor.Blue);
+        ConsoleU.WriteWithColor("Hello ", ConsoleColor.Magenta);
+        ConsoleU.WriteWithColor("there!", ConsoleColor.Cyan);
     }
 }
